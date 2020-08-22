6360283f560e0945b32d4c92339fa85e5b43cac3
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-08-22 17:05:18 +0600

    Query with hardcoded data

--- schema.js
+++ schema.js
@@ -7,7 +7,52 @@ const {
     GraphQLNoneNull
 } = require('graphql');
 
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt},
+    })
+});
+
+
+// Hardcoded data
+const customers = [
+    {id: '1', name: 'John Doe', email: 'jdoe@gmail', age: 35},
+    {id: '2', name: 'Steve Smith', email: 'steve@gmail', age: 25},
+    {id: '3', name: 'Sarah Williams', email: 'sarah@gmail', age: 32}
+];
+
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parentValue, args) {
+                for(let i = 0; i < customers.length; i++) {
+                    if(customers[i].id == args.id) {
+                        return customers[i];
+                    }
+                }
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return customers;
+            }
+        }
+    }
+});
 
 module.exports = new GraphQLSchema({
-    
+    query: RootQuery
 });
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 
 const schema = require('./schema.js');
 
 
 const app = express();
 
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
     schema: schema,
-    graphgiql: true
-}))
+    graphiql: true
+}));
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
-})
+});
